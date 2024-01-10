96cd7ccd04645c6cff88c141898e04ac2283c83e
Vanni Boncinelli <vanni.boncinelli@remira.com>
2024-01-10 12:39:11 +0100

    Add project files.

---
+++ PlayingWithSettings/Controllers/SettingsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlayingWithSettings.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SettingsController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpGet("{setting}")]
+        public IActionResult Get(string setting)
+        {
+            var value = this._configuration[setting];
+
+            if (value == null)
+                return BadRequest();
+
+            return Ok(value);
+        }
+    }
+}
